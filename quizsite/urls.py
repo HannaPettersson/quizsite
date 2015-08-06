0375e52d9768118c91f5cb2ebf77f13c84a50464
Hanna Pettersson <hanna@rebelandbird.com>
2015-08-06 13:51:57 +0200

    Created url names

--- quizsite/urls.py
+++ quizsite/urls.py
@@ -22,10 +22,10 @@ from quiz import views
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url("^$", views.startpage),
-	url(r"^quiz/([a-z-]+)/$", views.quiz),
-	url(r"^quiz/([a-z-]+)/question/([0-9])/$", views.question),
-	url(r"^quiz/([a-z-]+)/completed/$", views.completed),
+    url("^$", views.startpage, name= "start_page"),
+	url(r"^quiz/([a-z-]+)/$", views.quiz, , name = "quiz_page"),
+	url(r"^quiz/([a-z-]+)/question/([0-9])/$", views.question, name="question_page"),
+	url(r"^quiz/([a-z-]+)/completed/$", views.completed, name="completed_page"),
 ]
 
 
